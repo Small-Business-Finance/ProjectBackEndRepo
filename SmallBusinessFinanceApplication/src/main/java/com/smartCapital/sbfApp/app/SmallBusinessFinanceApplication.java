8a361b9959dc49d2a4bb48616c5704c1db9d44a8
payalsune <payalsune20@gmail.com>
2023-01-11 14:27:01 +0530

    first commit

---
+++ SmallBusinessFinanceApplication/src/main/java/com/smartCapital/sbfApp/app/SmallBusinessFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.smartCapital.sbfApp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallBusinessFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallBusinessFinanceApplication.class, args);
+	}
+
+}
---
+++ SmallBusinessFinanceApplication/src/test/java/com/smartCapital/sbfApp/app/SmallBusinessFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartCapital.sbfApp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallBusinessFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
