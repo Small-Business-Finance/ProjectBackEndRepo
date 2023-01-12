d94c2c724c7febaa6776df07a52f0c815dd85048
Liza Patel <lizapatel1212@gmail.com>
2023-01-12 05:34:34 -0500

    Enquiry controller class is created

---
+++ SmallBusinessFinanceApplication/src/main/java/com/smartCapital/sbfApp/app/service/EnquiryService.java
@@ -0,0 +1,27 @@
+package com.smartCapital.sbfApp.app.service;
+
+import com.smartCapital.sbfApp.app.model.CibilScore;
+import com.smartCapital.sbfApp.app.model.Enquiry;
+
+public interface EnquiryService {
+
+	void saveEnquiry(Enquiry enquiry);
+
+	void updateEnquiry(Integer id, Enquiry enquiry);
+
+	void saveCibilScore(CibilScore cibilscore);
+
+	void updateCibilScore(Integer id, CibilScore cibilscore);
+
+	void deleteEnquiry(Integer id);
+
+	Iterable<Enquiry> getEnquiry();
+
+	Enquiry getEnquirybyID(int id);
+
+	Iterable<CibilScore> getCibil();
+
+	CibilScore getCibilbyID(int id);
+	
+
+}
