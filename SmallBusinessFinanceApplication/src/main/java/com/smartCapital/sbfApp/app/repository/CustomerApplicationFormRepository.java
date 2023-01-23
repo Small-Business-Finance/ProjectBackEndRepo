2b63e0d5ef0a617557678f4f171826719b88a668
MaheshMasne <maheshmasne88@gmail.com>
2023-01-24 03:29:49 +0530

    DefaulterList

--- SmallBusinessFinanceApplication/src/main/java/com/smartCapital/sbfApp/app/repository/CustomerApplicationFormRepository.java
+++ SmallBusinessFinanceApplication/src/main/java/com/smartCapital/sbfApp/app/repository/CustomerApplicationFormRepository.java
@@ -1,6 +1,8 @@
 package com.smartCapital.sbfApp.app.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +13,7 @@ import com.smartCapital.sbfApp.app.model.CustomerLoanDetails;
 public interface CustomerApplicationFormRepository extends JpaRepository<CustomerApplicationForm, Integer>
 {
 	public CustomerApplicationForm findByApplicationId(Integer applicationId);
+	
+	public List<CustomerApplicationForm> findByCustomerLoanDetails_DefaultorCountGreaterThan(int defaultCount);
 	
 }
